acd973ac20ab9760528038440c4794e47f41d505
ahasanUllah <bulbulahasan@gmail.com>
2022-10-15 18:43:42 +0600

    email input method changed

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import app from '../firebase/firebase.init';
@@ -6,16 +6,23 @@ const auth = getAuth(app);
 const Login = () => {
    const [success, setSuccess] = useState();
    const [email, setEmail] = useState('');
+   const [user, setuser] = useState({});
    const handleLogin = (event) => {
       event.preventDefault();
       const form = event.target;
-      setEmail(form.email.value);
+      const userEmail = form.email.value;
       const password = form.password.value;
-      signInWithEmailAndPassword(auth, email, password)
+      signInWithEmailAndPassword(auth, userEmail, password)
          .then((res) => {
             const user = res.user;
-            setSuccess('Login Successfull');
+            const isVerified = user.emailVerified;
+            setuser(user);
             console.log(user);
+            if (!isVerified) {
+               alert('Please verify the email');
+               return;
+            }
+            setSuccess('Login Successfull');
          })
          .catch((error) => {
             console.log(error);
@@ -30,70 +37,167 @@ const Login = () => {
          alert('Reset Eamil sent');
       });
    };
+   const handleSignOut = () => {
+      signOut(auth)
+         .then(() => {
+            setuser({});
+            setSuccess('');
+            alert('sign out successfull');
+         })
+         .catch((error) => {
+            console.error(error);
+         });
+   };
    return (
       <div className="flex justify-center mt-20">
-         <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-gray-900 text-gray-100">
-            <h1 className="text-2xl font-bold text-center">Login</h1>
-            <form onSubmit={handleLogin} action="" className="space-y-6 ng-untouched ng-pristine ng-valid">
-               <div className="space-y-1 text-sm">
-                  <input
-                     type="email"
-                     name="email"
-                     id="email"
-                     placeholder="Email"
-                     className="w-full px-4 py-3 rounded-md border border-gray-700 bg-gray-900 text-gray-100 focus:border-violet-400"
-                  />
+         {!user.uid && (
+            <div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-gray-900 text-gray-100">
+               <h1 className="text-2xl font-bold text-center">Login</h1>
+               <form onSubmit={handleLogin} action="" className="space-y-6 ng-untouched ng-pristine ng-valid">
+                  <div className="space-y-1 text-sm">
+                     <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        placeholder="Email"
+                        className="w-full px-4 py-3 rounded-md border border-gray-700 bg-gray-900 text-gray-100 focus:border-violet-400"
+                     />
+                  </div>
+                  <div className="space-y-1 text-sm">
+                     <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="Password"
+                        className="w-full px-4 py-3 rounded-md border border-gray-700 bg-gray-900 text-gray-100 focus:border-violet-400"
+                     />
+                     <div className="flex justify-end text-xs text-gray-400">
+                        <button onClick={handleResetEmail} type="button" className="px-3 py-1">
+                           Forgot Password?
+                        </button>
+                     </div>
+                     <div>
+                        <p className="text-green-600">{success}</p>
+                     </div>
+                  </div>
+                  <button type="submit" className="block w-full p-3 text-center rounded-sm text-gray-900 bg-violet-400">
+                     Sign in
+                  </button>
+               </form>
+               <div className="flex items-center pt-4 space-x-1">
+                  <div className="flex-1 h-px sm:w-16 bg-gray-700"></div>
+                  <p className="px-3 text-sm text-gray-400">Login with social accounts</p>
+                  <div className="flex-1 h-px sm:w-16 bg-gray-700"></div>
                </div>
-               <div className="space-y-1 text-sm">
-                  <input
-                     type="password"
-                     name="password"
-                     id="password"
-                     placeholder="Password"
-                     className="w-full px-4 py-3 rounded-md border border-gray-700 bg-gray-900 text-gray-100 focus:border-violet-400"
+               <div className="flex justify-center space-x-4">
+                  <button aria-label="Log in with Google" className="p-3 rounded-sm">
+                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
+                        <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+                     </svg>
+                  </button>
+                  <button aria-label="Log in with Twitter" className="p-3 rounded-sm">
+                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
+                        <path d="M31.937 6.093c-1.177 0.516-2.437 0.871-3.765 1.032 1.355-0.813 2.391-2.099 2.885-3.631-1.271 0.74-2.677 1.276-4.172 1.579-1.192-1.276-2.896-2.079-4.787-2.079-3.625 0-6.563 2.937-6.563 6.557 0 0.521 0.063 1.021 0.172 1.495-5.453-0.255-10.287-2.875-13.52-6.833-0.568 0.964-0.891 2.084-0.891 3.303 0 2.281 1.161 4.281 2.916 5.457-1.073-0.031-2.083-0.328-2.968-0.817v0.079c0 3.181 2.26 5.833 5.26 6.437-0.547 0.145-1.131 0.229-1.724 0.229-0.421 0-0.823-0.041-1.224-0.115 0.844 2.604 3.26 4.5 6.14 4.557-2.239 1.755-5.077 2.801-8.135 2.801-0.521 0-1.041-0.025-1.563-0.088 2.917 1.86 6.36 2.948 10.079 2.948 12.067 0 18.661-9.995 18.661-18.651 0-0.276 0-0.557-0.021-0.839 1.287-0.917 2.401-2.079 3.281-3.396z"></path>
+                     </svg>
+                  </button>
+                  <button aria-label="Log in with GitHub" className="p-3 rounded-sm">
+                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
+                        <path d="M16 0.396c-8.839 0-16 7.167-16 16 0 7.073 4.584 13.068 10.937 15.183 0.803 0.151 1.093-0.344 1.093-0.772 0-0.38-0.009-1.385-0.015-2.719-4.453 0.964-5.391-2.151-5.391-2.151-0.729-1.844-1.781-2.339-1.781-2.339-1.448-0.989 0.115-0.968 0.115-0.968 1.604 0.109 2.448 1.645 2.448 1.645 1.427 2.448 3.744 1.74 4.661 1.328 0.14-1.031 0.557-1.74 1.011-2.135-3.552-0.401-7.287-1.776-7.287-7.907 0-1.751 0.62-3.177 1.645-4.297-0.177-0.401-0.719-2.031 0.141-4.235 0 0 1.339-0.427 4.4 1.641 1.281-0.355 2.641-0.532 4-0.541 1.36 0.009 2.719 0.187 4 0.541 3.043-2.068 4.381-1.641 4.381-1.641 0.859 2.204 0.317 3.833 0.161 4.235 1.015 1.12 1.635 2.547 1.635 4.297 0 6.145-3.74 7.5-7.296 7.891 0.556 0.479 1.077 1.464 1.077 2.959 0 2.14-0.020 3.864-0.020 4.385 0 0.416 0.28 0.916 1.104 0.755 6.4-2.093 10.979-8.093 10.979-15.156 0-8.833-7.161-16-16-16z"></path>
+                     </svg>
+                  </button>
+               </div>
+               <p className="text-xs text-center sm:px-6 text-gray-400">
+                  Don't have an account?
+                  <Link rel="noopener noreferrer" to="/register" className="underline text-gray-100">
+                     Sign up
+                  </Link>
+               </p>
+            </div>
+         )}
+
+         {/* User Profile */}
+         {user.uid && (
+            <div>
+               <div className="flex flex-col justify-center max-w-xs p-6 shadow-md rounded-xl sm:px-12 bg-gray-900 text-gray-100">
+                  <img
+                     src="https://source.unsplash.com/150x150/?portrait?3"
+                     alt=""
+                     className="w-32 h-32 mx-auto rounded-full bg-gray-500 aspect-square"
                   />
-                  <div className="flex justify-end text-xs text-gray-400">
-                     <button onClick={handleResetEmail} type="button" className="px-3 py-1">
-                        Forgot Password?
+                  <div className="space-y-4 text-center divide-y divide-gray-700">
+                     <div className="my-2 space-y-1">
+                        <h2 className="text-xl font-semibold sm:text-2xl">{user.displayName}</h2>
+                        <p className="px-5 text-xs sm:text-base text-gray-400">Full-stack developer</p>
+                     </div>
+                     <div className="flex justify-center pt-2 space-x-4 align-center">
+                        <a
+                           rel="noopener noreferrer"
+                           href="/"
+                           aria-label="GitHub"
+                           className="p-2 rounded-md text-gray-100 hover:text-violet-400"
+                        >
+                           <svg
+                              viewBox="0 0 496 512"
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="w-4 h-4 fill-current"
+                           >
+                              <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path>
+                           </svg>
+                        </a>
+                        <a
+                           rel="noopener noreferrer"
+                           href="/"
+                           aria-label="Dribble"
+                           className="p-2 rounded-md text-gray-100 hover:text-violet-400"
+                        >
+                           <svg
+                              viewBox="0 0 512 512"
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="w-4 h-4 fill-current"
+                           >
+                              <path d="M256 8C119.252 8 8 119.252 8 256s111.252 248 248 248 248-111.252 248-248S392.748 8 256 8zm163.97 114.366c29.503 36.046 47.369 81.957 47.835 131.955-6.984-1.477-77.018-15.682-147.502-6.818-5.752-14.041-11.181-26.393-18.617-41.614 78.321-31.977 113.818-77.482 118.284-83.523zM396.421 97.87c-3.81 5.427-35.697 48.286-111.021 76.519-34.712-63.776-73.185-116.168-79.04-124.008 67.176-16.193 137.966 1.27 190.061 47.489zm-230.48-33.25c5.585 7.659 43.438 60.116 78.537 122.509-99.087 26.313-186.36 25.934-195.834 25.809C62.38 147.205 106.678 92.573 165.941 64.62zM44.17 256.323c0-2.166.043-4.322.108-6.473 9.268.19 111.92 1.513 217.706-30.146 6.064 11.868 11.857 23.915 17.174 35.949-76.599 21.575-146.194 83.527-180.531 142.306C64.794 360.405 44.17 310.73 44.17 256.323zm81.807 167.113c22.127-45.233 82.178-103.622 167.579-132.756 29.74 77.283 42.039 142.053 45.189 160.638-68.112 29.013-150.015 21.053-212.768-27.882zm248.38 8.489c-2.171-12.886-13.446-74.897-41.152-151.033 66.38-10.626 124.7 6.768 131.947 9.055-9.442 58.941-43.273 109.844-90.795 141.978z"></path>
+                           </svg>
+                        </a>
+                        <a
+                           rel="noopener noreferrer"
+                           href="/"
+                           aria-label="Twitter"
+                           className="p-2 rounded-md text-gray-100 hover:text-violet-400"
+                        >
+                           <svg
+                              viewBox="0 0 512 512"
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="w-4 h-4 fill-current"
+                           >
+                              <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
+                           </svg>
+                        </a>
+                        <a
+                           rel="noopener noreferrer"
+                           href="/"
+                           aria-label="Email"
+                           className="p-2 rounded-md text-gray-100 hover:text-violet-400"
+                        >
+                           <svg
+                              viewBox="0 0 512 512"
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="w-4 h-4 fill-current"
+                           >
+                              <path d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"></path>
+                           </svg>
+                        </a>
+                     </div>
+                     <button
+                        onClick={handleSignOut}
+                        type="button"
+                        className="px-8 py-3 font-semibold border rounded border-gray-100 text-gray-100"
+                     >
+                        Sign out
                      </button>
                   </div>
-                  <div>
-                     <p className="text-green-600">{success}</p>
-                  </div>
                </div>
-               <button type="submit" className="block w-full p-3 text-center rounded-sm text-gray-900 bg-violet-400">
-                  Sign in
-               </button>
-            </form>
-            <div className="flex items-center pt-4 space-x-1">
-               <div className="flex-1 h-px sm:w-16 bg-gray-700"></div>
-               <p className="px-3 text-sm text-gray-400">Login with social accounts</p>
-               <div className="flex-1 h-px sm:w-16 bg-gray-700"></div>
             </div>
-            <div className="flex justify-center space-x-4">
-               <button aria-label="Log in with Google" className="p-3 rounded-sm">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
-                     <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
-                  </svg>
-               </button>
-               <button aria-label="Log in with Twitter" className="p-3 rounded-sm">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
-                     <path d="M31.937 6.093c-1.177 0.516-2.437 0.871-3.765 1.032 1.355-0.813 2.391-2.099 2.885-3.631-1.271 0.74-2.677 1.276-4.172 1.579-1.192-1.276-2.896-2.079-4.787-2.079-3.625 0-6.563 2.937-6.563 6.557 0 0.521 0.063 1.021 0.172 1.495-5.453-0.255-10.287-2.875-13.52-6.833-0.568 0.964-0.891 2.084-0.891 3.303 0 2.281 1.161 4.281 2.916 5.457-1.073-0.031-2.083-0.328-2.968-0.817v0.079c0 3.181 2.26 5.833 5.26 6.437-0.547 0.145-1.131 0.229-1.724 0.229-0.421 0-0.823-0.041-1.224-0.115 0.844 2.604 3.26 4.5 6.14 4.557-2.239 1.755-5.077 2.801-8.135 2.801-0.521 0-1.041-0.025-1.563-0.088 2.917 1.86 6.36 2.948 10.079 2.948 12.067 0 18.661-9.995 18.661-18.651 0-0.276 0-0.557-0.021-0.839 1.287-0.917 2.401-2.079 3.281-3.396z"></path>
-                  </svg>
-               </button>
-               <button aria-label="Log in with GitHub" className="p-3 rounded-sm">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
-                     <path d="M16 0.396c-8.839 0-16 7.167-16 16 0 7.073 4.584 13.068 10.937 15.183 0.803 0.151 1.093-0.344 1.093-0.772 0-0.38-0.009-1.385-0.015-2.719-4.453 0.964-5.391-2.151-5.391-2.151-0.729-1.844-1.781-2.339-1.781-2.339-1.448-0.989 0.115-0.968 0.115-0.968 1.604 0.109 2.448 1.645 2.448 1.645 1.427 2.448 3.744 1.74 4.661 1.328 0.14-1.031 0.557-1.74 1.011-2.135-3.552-0.401-7.287-1.776-7.287-7.907 0-1.751 0.62-3.177 1.645-4.297-0.177-0.401-0.719-2.031 0.141-4.235 0 0 1.339-0.427 4.4 1.641 1.281-0.355 2.641-0.532 4-0.541 1.36 0.009 2.719 0.187 4 0.541 3.043-2.068 4.381-1.641 4.381-1.641 0.859 2.204 0.317 3.833 0.161 4.235 1.015 1.12 1.635 2.547 1.635 4.297 0 6.145-3.74 7.5-7.296 7.891 0.556 0.479 1.077 1.464 1.077 2.959 0 2.14-0.020 3.864-0.020 4.385 0 0.416 0.28 0.916 1.104 0.755 6.4-2.093 10.979-8.093 10.979-15.156 0-8.833-7.161-16-16-16z"></path>
-                  </svg>
-               </button>
-            </div>
-            <p className="text-xs text-center sm:px-6 text-gray-400">
-               Don't have an account?
-               <Link rel="noopener noreferrer" to="/register" className="underline text-gray-100">
-                  Sign up
-               </Link>
-            </p>
-         </div>
+         )}
       </div>
    );
 };
